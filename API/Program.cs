c23f71543bde74316d11822244a80ca8168fbf81
Ronit Ramnarayan <ronitvjramnarayan@gmail.com>
2025-01-15 17:02:48 -0500

    Created client side with Angular.

--- API/Program.cs
+++ API/Program.cs
@@ -3,13 +3,16 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddControllers();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
