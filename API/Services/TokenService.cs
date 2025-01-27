ab796d799dc3bb5e0c1531dbcb05b6ea8ce38207
Ronit Ramnarayan <ronitvjramnarayan@gmail.com>
2025-01-27 18:58:38 -0500

    Created createToken method

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,18 +1,41 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Interfaces;
-
+using Microsoft.IdentityModel.Tokens;
 using API.Models;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace API.Services
 {
-    public class TokenService : ITokenService
+    public class TokenService(IConfiguration config) : ITokenService
     {
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
-        }
+            var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from appsettings");
+            if (tokenKey.Length < 64) throw new Exception("Your tokenKey needs to be longer");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+            var claims = new List<Claim>{
+                new(ClaimTypes.NameIdentifier, user.UserName)
+            };
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
     }
+}
 }
